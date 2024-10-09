e13b700fa9ec6277bc9dcdb82ba357aaa7534711
Sam1713 <samsaju399@gmail.com>
2024-10-09 09:07:58 +0530

    one more port added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -89,6 +89,7 @@ app.use('/backend/contact', contactRoute);
 app.use('/backend/quiz',quizRoute)
 app.use('/backend/chat',chatRoute)
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+const PORT=process.env.PORT||3001
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
