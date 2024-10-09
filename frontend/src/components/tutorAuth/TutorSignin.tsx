833343a6a128c531547bf369caf602d9b8106842
Sam1713 <samsaju399@gmail.com>
2024-10-09 22:38:19 +0530

    route changed

--- frontend/src/components/tutorAuth/TutorSignin.tsx
+++ frontend/src/components/tutorAuth/TutorSignin.tsx
@@ -31,7 +31,7 @@ const TutorSignin:React.FC=()=> {
         console.log("form",formdata)
       console.log('Form submitted');
       console.log(values);
-      const response=await api.post("/backend/tutor/tutorsignin",values)
+      const response=await api.post("https://proxima.ec-shop.life/backend/tutor/tutorsignin",values)
             console.log('res',response.data)
 
       localStorage.setItem('tutor_access_token', response.data.token);
