7dedda48dd9fee3a17fa17ff45f56d5dcc7db9e5
Sam1713 <samsaju399@gmail.com>
2024-10-09 12:06:23 +0530

    baseurl changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Replace with your frontend's URL
+    origin: 'http://localhost:5173'||"https://proxima-elearn-4iq5ubxc1-sam1713s-projects.vercel.app/", // Replace with your frontend's URL
     methods: ['GET', 'POST'],
     credentials: true
   }
