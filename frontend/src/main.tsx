cd3df1b941cd50d14c8e84829640dc2dfbc2734e
Sam1713 <samsaju399@gmail.com>
2024-10-10 09:01:25 +0530

    crypto browserify added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import 'webcrypto-shim';
+import 'crypto-browserify';
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,6 +7,11 @@ import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/es/integration/react'
 import { ThemeProvider } from "@material-tailwind/react";
 
+if (!window.crypto || !window.crypto.getRandomValues) {
+  console.error('Crypto API not available or unsupported. Make sure your environment supports the Web Crypto API.');
+} else {
+  console.log('Crypto API is available and supported.');
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
